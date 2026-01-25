ee9a535374703da5e5c4f9a0d56a86a0c27f735e
Thiago Lima <thi4got9lima@gmail.com>
2026-01-25 16:53:12 -0300

    feat(ViewModel): implementa a classe DisciplinaModel

---
+++ Codigo/VemCaProf/VemCaProfWeb/Models/DisciplinaModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VemCaProfWeb.Models
+{
+    public class DisciplinaModel
+    {
+        [Display(Name = "Código")]
+        [Key]
+        [Required(ErrorMessage = "O campo Código é obrigatório.")]
+        public uint Id { get; set; }
+
+        [Display(Name = "Nome da disciplina")]
+        [Required(ErrorMessage = "Campo requirido.")]
+        [StringLength(45, MinimumLength = 5 ,ErrorMessage = "O nome da disciplina não pode exceder 45 caracteres.")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Display(Name = "Descrição da disciplina")]
+        [StringLength(200, ErrorMessage = "A descrição da disciplina não pode exceder 200 caracteres.")]
+        public string? Descricao { get; set; }
+
+        [Display(Name = "Nível da disciplina")]
+        [StringLength(2, ErrorMessage = "O nível da disciplina deve ter no máximo 2 caracteres.")]
+        public string? Nivel { get; set; }
+    }
+}
