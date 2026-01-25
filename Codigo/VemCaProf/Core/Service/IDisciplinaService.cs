0abe9d08c5fe6a440eb6b3f1879871735cd8f1e3
Thiago Lima <thi4got9lima@gmail.com>
2026-01-25 02:19:53 -0300

    feat: adição da interface disciplina

---
+++ Codigo/VemCaProf/Core/Service/IDisciplinaService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    internal interface IDisciplinaService
+    {
+        uint Create(Disciplina disciplina);
+        void Edit(Disciplina disciplina);
+        void Delete(uint id);
+        Disciplina Get(uint id);
+        IEnumerable<Disciplina> GetAll();
+        IEnumerable<Disciplina> GetByNome(string nome);
+
+    }
+}
