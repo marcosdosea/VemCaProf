8c56bdaa85dbcf12e3516ba3fe8f2cc291453fe6
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-01-27 15:55:16 -0300

    adicionando PenalidadeDTO.cs #128

---
+++ Codigo/VemCaProf/Core/DTO/PenalidadeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTO
+{
+    public class PenalidadeDTO : PessoaDTO
+    {
+        public int id { get; set; }
+        public DateTime dataHorario { get; set; }
+        public string descricao { get; set; } = null!;
+    }
+}
