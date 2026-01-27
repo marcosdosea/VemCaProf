2c53ddd09c26106c76bc78bc02f54e0ca1f4da24
Gabr1elFraga <gabrielsilveirafraga@hotmail.com>
2026-01-26 22:57:58 -0300

    Ajustando PessoaProfile
    Retirado comentarios desnecessarios.

--- Codigo/VemCaProf/VemCaProfWeb/Mappers/PessoaProfile.cs
+++ Codigo/VemCaProf/VemCaProfWeb/Mappers/PessoaProfile.cs
@@ -1,36 +1,26 @@
 using AutoMapper;
-using Core;          // <--- Adicione isso para enxergar a classe 'Pessoa'
+using Core;          
 using Core.DTO;
 using VemCaProfWeb.Models;
 
-namespace VemCaProfWeb.Mappers // Recomendo usar o namespace completo do projeto
+namespace VemCaProfWeb.Mappers 
 {
     public class PessoaProfile : Profile
     {
         public PessoaProfile()
         {
-            // ==========================================================
-            // 1. LEITURA (Do Banco para a Tela)
-            // Entity (Pessoa) -> ViewModel
-            // ==========================================================
-            
-            // Resolve o erro: Missing map configuration Pessoa -> ResponsavelPessoaModel
+
             CreateMap<Pessoa, ResponsavelPessoaModel>();
             
             CreateMap<Pessoa, AlunoPessoaModel>()
-                // Se o nome da propriedade for diferente (ex: ResponsavelId vs IdResponsavel), precisa explicar:
                 .ForMember(dest => dest.IdResponsavel, opt => opt.MapFrom(src => src.ResponsavelId));
 
             CreateMap<Pessoa, ProfessorPessoaModel>();
-
-
-            // ==========================================================
-            // 2. ESCRITA (Da Tela para o Serviço)
-            // ViewModel -> DTO
-            // ==========================================================
             
             CreateMap<ProfessorPessoaModel, ProfessorPessoaDTO>().ReverseMap();
+            
             CreateMap<AlunoPessoaModel, AlunoPessoaDTO>().ReverseMap();
+            
             CreateMap<ResponsavelPessoaModel, ResponsavelPessoaDTO>().ReverseMap();
         }
     }
