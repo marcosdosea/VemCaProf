3bf9f7d42f70bcad5a61a477ad4050ce12498017
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-01-27 17:12:27 -0300

    inserindo PenalidadeProfile.cs #129

--- Codigo/VemCaProf/VemCaProfWeb/Mappers/PenalidadeProfile.cs
+++ Codigo/VemCaProf/VemCaProfWeb/Mappers/PenalidadeProfile.cs
@@ -1,6 +1,14 @@
-﻿namespace VemCaProfWeb.Mappers
+﻿using AutoMapper;
+using Core;
+using VemCaProfWeb.Models;
+
+namespace Mappers
 {
-    public class PenalidadeProfile
+    public class PenalidadeProfile : Profile
     {
+        public PenalidadeProfile()
+        {
+            CreateMap<PenalidadeModel, Penalidade>().ReverseMap();
+        }
     }
 }
