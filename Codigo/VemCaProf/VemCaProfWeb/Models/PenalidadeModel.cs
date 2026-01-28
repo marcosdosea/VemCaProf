9d527bfd5aac22699f0de00004013dd240698f39
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-01-28 20:07:22 -0300

    Implementando View Model #130

--- Codigo/VemCaProf/VemCaProfWeb/Models/PenalidadeModel.cs
+++ Codigo/VemCaProf/VemCaProfWeb/Models/PenalidadeModel.cs
@@ -7,5 +7,23 @@ namespace VemCaProfWeb.Models
         [Display(Name = "Código da penalidade")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int Id { get; set; }
+
+        [Display(Name = "Data e hora de início")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = "O campo deve apresentar uma data e hora válida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime DataHorarioInicio { get; set; }
+
+        [Display(Name = "Data e hora de fim")]
+
+        [DataType(DataType.DateTime,ErrorMessage = "O campo deve apresentar uma data e hora válida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime? DataHoraFim { get; set; }
+
+        [Display(Name = "Tipo de penalidade")]
+        public string? Tipo { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; } = null!;
     }
 }
