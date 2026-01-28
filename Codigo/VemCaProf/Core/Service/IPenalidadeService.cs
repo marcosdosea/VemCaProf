5aeb2b904f8b348ad85278b91d1ac8f67edae0cb
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-01-28 19:35:44 -0300

    Implementando interface Penalidade e classe PenalidadeService com os métodos de criar,remover e editar prontos #126 #128

---
+++ Codigo/VemCaProf/Core/Service/IPenalidadeService.cs
@@ -0,0 +1,15 @@
+﻿using Core.DTO;
+using System.Collections.Generic;
+
+
+namespace Core.Service
+{
+    public interface IPenalidadeService
+    {
+        int Create(PenalidadeDTO penalidade);
+        void Delete(int id);
+        void Edit(PenalidadeDTO penalidade);
+        IEnumerable<PenalidadeDTO> GetAll();
+        IEnumerable<PenalidadeDTO> GetByPessoaId(int pessoaId);
+    }
+}
---
+++ Codigo/VemCaProf/Service/PenalidadeService.cs
@@ -0,0 +1,77 @@
+﻿using Core;
+using Core.DTO;
+using Core.Mappers;
+using Core.Service;
+
+namespace Service
+{
+    public class PenalidadeService : IPenalidadeService
+    {
+        private readonly VemCaProfContext _context;
+
+        public PenalidadeService(VemCaProfContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Criando uma nova penalidade na base de dados
+        ///</summary>
+        ///<param name="penalidade"></param>
+        ///<returns> id do autor </returns>
+        public int Create(PenalidadeDTO penalidade)
+        {
+           _context.Add(penalidade);
+           _context.SaveChanges();
+           return penalidade.Id;
+        }
+
+        /// <summary>
+        /// Deletar uma penalidade pelo ID
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(int id)
+        {
+            var penalidade = _context.Penalidades.Find(id);
+            if (penalidade != null)
+            {
+               _context.Remove(penalidade);
+               _context.SaveChanges();
+
+            }
+                
+            
+        }
+
+        /// <summary>
+        /// Editar penalidade existente
+        /// </summary>
+        /// <param name="penalidadeNova"></param>
+        public void Edit(PenalidadeDTO penalidadeNova)
+        {
+            if(penalidadeNova.Id == 0
+                throw new ServiceException("ID da penalidade inválido.");
+
+            var penalidadeExistente = _context.Penalidades.Find(penalidadeNova.Id);
+            if (penalidadeExistente == null)
+                throw new ServiceException("Penalidade não encontrada.");
+
+            
+            _context.Update(penalidadeNova);
+            _context.SaveChanges();
+
+        }
+
+        public IEnumerable<PenalidadeDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PenalidadeDTO> GetByPessoaId(int pessoaId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
