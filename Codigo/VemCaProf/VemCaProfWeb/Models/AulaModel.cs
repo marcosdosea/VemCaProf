ed4955e535e31c2fc2e985dd1867646e12047fc4
alidamittaraquis <127056873+alidamittaraquis@users.noreply.github.com>
2026-01-31 21:09:56 -0300

    model

---
+++ Codigo/VemCaProf/VemCaProfWeb/Models/AulaModel.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace VemCaProfWeb.Models;
+
+public class AulaModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Horário início é obrigatório")]
+    [Display(Name = "HorarioInicio")]
+    public DateTime DataHorarioInicio { get; set; }
+
+    [Required(ErrorMessage = "Horário final é obrigatório")]
+    [Display(Name = "HorarioFinal")]
+    public DateTime DataHorarioFinal { get; set; }
+
+    [Required(ErrorMessage = "Descrição é obrigatório")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "Descrição deve ter entre 2 e 100 caracteres")]
+    [Display(Name = "Descrição")]
+    public string Descricao { get; set; } = null!;
+
+
+
+
+}
