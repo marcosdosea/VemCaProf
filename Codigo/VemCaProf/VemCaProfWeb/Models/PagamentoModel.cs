e6d236ad5a039c844e612e9b8db2058c740d33b2
Luis Gustavo Alves Correia <gucorreia2901@gmail.com>
2026-02-05 10:51:49 -0300

    adicionado model de pagamento

---
+++ Codigo/VemCaProf/VemCaProfWeb/Models/PagamentoModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VemCaProfWeb.Models
+{
+    public class PagamentoModel
+    {
+        [Required]
+        [Display(Name = "ID da Aula")]
+        public int IdAula { get; set; }
+
+        [Display(Name = "Descrição da Aula")]
+        public string DescricaoAula { get; set; } = "";
+
+        [Display(Name = "Valor")]
+        [DataType(DataType.Currency)]
+        public double Valor { get; set; }
+
+        [Required]
+        [Display(Name = "Método de Pagamento")]
+        public string MetodoPagamento { get; set; } = null!;
+    }
+}
