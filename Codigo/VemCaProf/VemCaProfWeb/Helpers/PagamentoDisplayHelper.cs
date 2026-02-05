3f176df7243a944a3cbddeeb482106e6bc9eb01d
Luis Gustavo Alves Correia <gucorreia2901@gmail.com>
2026-02-05 10:52:17 -0300

    feat: adicionado helper para mapear o status e o metodo de pagamento

---
+++ Codigo/VemCaProf/VemCaProfWeb/Helpers/PagamentoDisplayHelper.cs
@@ -0,0 +1,24 @@
+﻿using Core.Enums;
+
+namespace VemCaProfWeb.Helpers
+{
+    public static class PagamentoDisplayHelper
+    {
+        public static string StatusLabel(string status) => status switch
+        {
+            StatusEnum.Agendada => "Agendada",
+            StatusEnum.Realizada => "Realizada",
+            StatusEnum.Paga => "Paga",
+            StatusEnum.AguardandoPagamento => "Pendente",
+            _ => status
+        };
+
+        public static string MetodoLabel(string metodo) => metodo switch
+        {
+            MetodoPagamentoEnum.Pix => "Pix",
+            MetodoPagamentoEnum.Credito => "Crédito",
+            MetodoPagamentoEnum.Debito => "Débito",
+            _ => metodo
+        };  
+    }
+}
